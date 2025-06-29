22d498ea50ceaece9589df2fdb715ffe4bc1c294
AlexAksioma <alexmedqwerty@gmail.com>
2025-06-29 20:56:05 +0300

    added calendar

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -8,9 +9,9 @@ import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
 
 import java.time.LocalDate;
 
-public class HomePage extends BasePage{
+public class HomePage extends BasePage {
 
-    public HomePage(WebDriver driver){
+    public HomePage(WebDriver driver) {
         setDriver(driver);
         driver.get("https://ilcarro.web.app/search");
         PageFactory.initElements(
@@ -25,19 +26,47 @@ public class HomePage extends BasePage{
     WebElement inputCity;
     @FindBy(id = "dates")
     WebElement inputDates;
+    @FindBy(xpath = "//button[@type='submit']")
+    WebElement btnYalla;
 
-    public void clickBtnLoginHeader(){
+    //======== calendar ===========
+    @FindBy(xpath = "//button[@aria-label='Choose month and year']")
+    WebElement btnMonthYear;
+
+    public void clickBtnLoginHeader() {
         btnLoginHeader.click();
     }
 
     //   7/10/2025 - 8/30/2025
-    public void typeSearchForm(String city, LocalDate startDate, LocalDate endDate){
+    public void typeSearchForm(String city, LocalDate startDate, LocalDate endDate) {
         inputCity.sendKeys(city);
-        inputDates.sendKeys(dateToString(startDate)+" / "+dateToString(endDate));
+        inputDates.sendKeys(dateToString(startDate) + " - " + dateToString(endDate));
+        removeDisabledBtnSearch();
+        btnYalla.click();
     }
 
-    private String dateToString(LocalDate date){
-        return (date.getMonthValue()) +"/"+date.getDayOfMonth()+"/"+date.getYear();
+    private String dateToString(LocalDate date) {
+        return (date.getMonthValue()) + "/" + date.getDayOfMonth() + "/" + date.getYear();
+    }
+
+    public void typeSearchFormCalendar(String city, LocalDate startDate, LocalDate endDate) {
+        inputCity.sendKeys(city);
+        inputDates.click();
+        typeYearMonthDay(startDate);
+        typeYearMonthDay(endDate);
+        removeDisabledBtnSearch();
+        btnYalla.click();
+    }
+
+    private void typeYearMonthDay(LocalDate date) {
+        btnMonthYear.click();
+        String year = Integer.toString(date.getYear());  //2025
+        driver.findElement(By.xpath("//div[contains(text(),'" + year + "')]")).click();//div[contains(text(),'2025')]
+        String month = date.getMonth().toString();
+        System.out.println(month.substring(0,3));
+        driver.findElement(By.xpath("//div[contains(text(),'" + month.substring(0,3) + "')]")).click();
+        String day = String.valueOf(date.getDayOfMonth());
+        driver.findElement(By.xpath("//div[contains(text(),'" + day + "')]")).click();
     }
 
 
--- src/test/java/ui_tests/SearchTests.java
+++ src/test/java/ui_tests/SearchTests.java
@@ -1,8 +1,10 @@
 package ui_tests;
 
 import manager.ApplicationManager;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.HomePage;
+import pages.ResultsPage;
 
 import java.time.LocalDate;
 
@@ -11,6 +13,22 @@ public class SearchTests extends ApplicationManager {
     public void searchPositiveTest(){
         HomePage homePage = new HomePage(getDriver());
         homePage.typeSearchForm("Haifa", LocalDate.of(2025, 7, 25),
+                LocalDate.of(2025, 8, 25));
+        Assert.assertTrue(new ResultsPage(getDriver()).validateUrl("results"));
+    }
+
+    @Test
+    public void searchPositiveTestCalendar(){
+        HomePage homePage = new HomePage(getDriver());
+        homePage.typeSearchFormCalendar("Haifa", LocalDate.of(2025, 7, 25),
+                LocalDate.of(2025, 8, 25));
+        Assert.assertTrue(new ResultsPage(getDriver()).validateUrl("results"));
+    }
+
+    @Test(expectedExceptions = {org.openqa.selenium.NoSuchElementException.class})
+    public void searchNegativeTestCalendar(){
+        HomePage homePage = new HomePage(getDriver());
+        homePage.typeSearchFormCalendar("Haifa", LocalDate.of(2025, 5, 25),
                 LocalDate.of(2025, 8, 25));
     }
 }
